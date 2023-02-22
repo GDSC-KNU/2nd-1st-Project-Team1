24486bf662c18fd31670f0e74ab60a7fc17a8d09
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-02-23 01:47:25 +0900

    chore(ci): add deploy to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 // https://vitejs.dev/config/
 
 export default defineConfig({
+  base: "./",
   plugins: [react(), vanillaExtractPlugin()],
   test: {
     globals: true,
