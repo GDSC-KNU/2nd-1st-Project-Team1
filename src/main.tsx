d1b78dc2af3795a946ede2b17bac520bd7bf9b90
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-03-02 19:03:03 +0900

    chore(react-router): add react-router w/ basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import Root from "./routes/root";
+import Page from "./routes/page";
+
+const route = createBrowserRouter(
+ createRoutesFromElements(
+  <Route path="/" element={<Root />}>
+    <Route path="/" element={<App />} />
+    <Route path="/page" element={<Page />} />
+    <Route path="*" element={<div>Not Found</div>} />
+  </Route>
+ )
+, { basename: "/2nd-1st-Project-Team1/" });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={route} />
   </React.StrictMode>
 );
