69e82cc59e73fa77c48aa927972c2f25a915a8a8
김현우 <krokerdile@naver.com>
2023-03-05 20:11:29 +0900

    Revert "chore: storybook setting"
    This reverts commit 56c93be7a3301e4e437583847d94105d7625120f.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,6 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-
-// 테스트 단위 
 describe("App", () => {
   it.skip("should renders App component", () => {
     render(<App />);
