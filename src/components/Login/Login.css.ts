c7c55d0c4bb017615a0d9782e31399f134f315cc
Yewon Cha <yewon51317@naver.com>
2023-03-12 16:45:21 +0900

    feat(login): create signin button

--- src/components/Login/Login.css.ts
+++ src/components/Login/Login.css.ts
@@ -1,1 +1,23 @@
 import { style } from "@vanilla-extract/css";
+
+export const loginButton = style({
+  userSelect: "none",
+  cursor: "pointer",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "36px",
+  borderStyle: "solid",
+  borderWidth: "1px",
+  borderRadius: "4px",
+  borderColor: "#f75c5c",
+  color: "#f75c5c",
+  fontSize: "16px",
+  lineHeight: "1",
+  paddingLeft: "12px",
+  paddingRight: "12px",
+  background: "#faeded",
+  marginTop: "6px",
+  marginBottom: "12px",
+  width: "100%",
+});
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,10 @@
+import {loginButton} from "./Login.css"
+
 const Login = () => {
     return (
         <div>
             <h1>CAN I GRADUATE?</h1>
+            <button className={loginButton}>Sign in</button>
         </div>
     );
 };
