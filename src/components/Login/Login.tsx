0f7176c83cd19f2b294c9f816c6ad6f7e2eb1c38
thumbzzero <thumbzzero@gmail.com>
2023-03-12 16:53:48 +0900

    fix(login): solve conflict

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,12 +1,43 @@
-import {loginButton} from "./Login.css"
+import { useState } from "react";
+import { loginContainer, capImage, input, loginButton, signUpGuide, signUpLink } from "./Login.css";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>CAN I GRADUATE?</h1>
-            <button className={loginButton}>Sign in</button>
-        </div>
-    );
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  const updateId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value);
+  };
+
+  const updatePw = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPw(e.target.value);
+  };
+
+  return (
+    <div className={loginContainer}>
+      <img className={capImage} src="img/graduation_cap.png" alt="" />
+      <h1>CAN I GRADUATE?</h1>
+      <form>
+        <input
+          className={input}
+          type="text"
+          placeholder="Enter your Student ID..."
+          value={id}
+          onChange={updateId}
+        />
+        <input
+          className={input}
+          type="password"
+          placeholder="Enter your Password..."
+          value={pw}
+          onChange={updatePw}
+        />
+        <button className={loginButton}>Sign in</button>
+      </form>
+      <span className={signUpGuide}>New to CAN I GRADUATE?</span>
+      <span className={signUpLink}>Create an account.</span>
+    </div>
+  );
 };
 
 export default Login;
