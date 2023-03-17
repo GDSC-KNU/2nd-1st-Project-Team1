a6944addebbd0d220ccae32c9440753085a44203
krokerdile <crazykroker@gmail.com>
2023-03-17 17:03:08 +0900

    feat: select 추가 - 일단 select로 작업해두긴 했는데 key value로 받을 꺼 생각하면 입력으로 바꿔야 되나 싶기도하고...?

---
+++ src/components/Planner/Modal.css.ts
@@ -0,0 +1,10 @@
+import { style } from "@vanilla-extract/css";
+
+export const AddSemesterModalBox = style({
+  backgroundColor: "white",
+  width: "20rem",
+  padding: "2rem",
+  display: "flex",
+  flexDirection: "column",
+  gap: "0.5rem",
+});
