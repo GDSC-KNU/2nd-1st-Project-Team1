c096f6bd50946107ebf1377b8330cf65aeabe142
krokerdile <crazykroker@gmail.com>
2023-03-17 17:03:08 +0900

    feat: select 추가 - 일단 select로 작업해두긴 했는데 key value로 받을 꺼 생각하면 입력으로 바꿔야 되나 싶기도하고...?

--- src/components/Planner/AddSemesterModal.tsx
+++ src/components/Planner/AddSemesterModal.tsx
@@ -1,40 +1,47 @@
 import BackgroundModal from "../Modal/BackgroundModal";
+import { AddSemesterModalBox } from "./Modal.css";
 
 const AddSemesterModal = ({ openModal, setOpenModal }: any) => {
   return (
     <BackgroundModal
-      width="400"
+      width="440"
       p="1rem"
       openModal={openModal}
       setOpenModal={setOpenModal}
     >
-      <>
+      <div className={AddSemesterModalBox}>
         <h1>학기</h1>
         <select>
-          <option key="banana" value="banana">
-            바나나
+          <option key="1st" value="1">
+            1학년
           </option>
-          <option key="apple" value="apple">
-            사과
+          <option key="2nd" value="apple">
+            2학년
           </option>
-          <option key="orange" value="orange">
-            오렌지
+          <option key="3rd" value="orange">
+            3학년
+          </option>
+          <option key="3rd" value="orange">
+            4학년
           </option>
         </select>
         <h1>학기</h1>
         <select>
           <option key="banana" value="banana">
-            바나나
+            1학기
           </option>
           <option key="apple" value="apple">
-            사과
+            2학기
           </option>
           <option key="orange" value="orange">
-            오렌지
+            여름학기
+          </option>
+          <option key="orange" value="orange">
+            겨율학기
           </option>
         </select>
         <button>Submit</button>
-      </>
+      </div>
     </BackgroundModal>
   );
 };
