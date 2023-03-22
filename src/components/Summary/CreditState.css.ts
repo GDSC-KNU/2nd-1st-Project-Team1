095b2ad6e875b82512525e483ff55f8f0a82072c
thumbzzero <thumbzzero@gmail.com>
2023-03-22 14:44:24 +0900

    feat(summary): add credit state table

--- src/components/Summary/CreditState.css.ts
+++ src/components/Summary/CreditState.css.ts
@@ -3,4 +3,30 @@ import { style } from "@vanilla-extract/css";
 export const border = style({
     border: "3px solid yellow",
     height: "60vh",
-  });
+});
+
+export const CreditType = style({
+    backgroundColor: "#FF000080",
+    border: "none",
+    borderRadius: "5px",
+    padding: "0.5rem 1rem",
+    color: "white",
+    width: "5rem",
+})
+
+export const Table = style({
+    width: "90%",
+    textAlign: "center",
+    borderSpacing: "0.5rem 1rem",
+    // borderCollapse: "collapse",    
+});
+
+export const TableBorder = style({
+    borderTop: "1px solid #D9D9D9",
+    padding: "0.7rem",
+});
+
+export const CreditSpan = style({
+    fontWeight: "bold",
+    fontSize: "1.5rem",
+});
--- src/components/Summary/CreditState.tsx
+++ src/components/Summary/CreditState.tsx
@@ -1,10 +1,55 @@
-import { border } from "./CreditState.css";
+import { border, CreditSpan, CreditType, Table, TableBorder } from "./CreditState.css";
+import { useState } from 'react';
 
 
 const CreditState = () => {
+
     return (
         <div className={border}>
-            
+            <table className={Table}>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>남은 학점</th>
+                        <th>들은 학점</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><button className={CreditType}>기본소양</button></td>
+                        <td><span className={CreditSpan}>0</span> 학점</td>
+                        <td><span className={CreditSpan}>0</span> 학점</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td><button className={CreditType}>전공기반</button></td>
+                        <td><span className={CreditSpan}>0</span> 학점</td>
+                        <td><span className={CreditSpan}>0</span> 학점</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td><button className={CreditType}>공학전공</button></td>
+                        <td><span className={CreditSpan}>12</span> 학점</td>
+                        <td><span className={CreditSpan}>12</span> 학점</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td><button className={CreditType}>기타</button></td>
+                        <td><span className={CreditSpan}>0</span> 학점</td>
+                        <td><span className={CreditSpan}>0</span> 학점</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td className={TableBorder}>총 <span className={CreditSpan}>0</span> 학점</td>
+                        <td className={TableBorder}>총 <span className={CreditSpan}>0</span> 학점</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
