cd4de88e3079ff518e3b19899f9b0cde498da1a5
Yewon Cha <yewon51317@naver.com>
2023-03-23 12:25:59 +0900

    feat: add checkbox

--- src/components/Summary/CheckBox.css.ts
+++ src/components/Summary/CheckBox.css.ts
@@ -1,6 +1,39 @@
 import { style } from "@vanilla-extract/css";
+import CheckBox from "./CheckBox";
 
 export const border = style({
     border: "3px solid green",
     height: "60vh",
   });
+
+export const input = style({
+  width: "30px",
+  height: "30px",
+  marginRight: "10px",
+  marginLeft: "10px",
+  accentColor: "#f75c5c",
+})
+
+export const CheckOption = style({
+  display: "inline-block",
+  width: "200px",
+  height: "30px",
+  lineHeight: "200px",
+  position: "relative",
+  top: "-10px",
+})
+
+export const upDown = style({
+  display: "inline-block",
+  width: "15px",
+  height: "15px",
+  position: "relative",
+  top: "2px",
+  marginLeft: "2px",
+  marginRight: "2px",
+  textAlign: "center",
+})
+
+export const counting = style({
+  width: "50px",
+})
--- src/components/Summary/CheckBox.tsx
+++ src/components/Summary/CheckBox.tsx
@@ -1,10 +1,23 @@
-import { border } from "./CheckBox.css";
-
+import { border, input, CheckOption, upDown, counting } from "./CheckBox.css";
 
 const CheckBox = () => {
     return (
         <div className={border}>
-            
+            <div>
+                <input className={input} type="checkbox" /><label className={CheckOption}>첨성인 기초</label>
+                <input className={input} type="checkbox" /><label className={CheckOption}>첨성인 핵심 - 인문사회</label>
+                <input className={input} type="checkbox" /><label className={CheckOption}>첨성인 핵심 - 자연과학</label>
+                <input className={input} type="checkbox" /><label className={CheckOption}>인문교양</label>
+            </div>
+            <div>
+                <input className={input} type="checkbox" /><label className={CheckOption}>어학성적</label>
+                <input className={input} type="checkbox" /><label className={CheckOption}>현장실습</label>
+                <input className={input} type="checkbox" /><label className={CheckOption}>지도교수 상담
+                </label>
+                <input className={counting} type="text" value={"0"}></input>
+                <input className={upDown} type="button" value={"+"}></input>
+                <input className={upDown} type="button" value={"-"}></input>
+            </div>
         </div>
     );
 };
