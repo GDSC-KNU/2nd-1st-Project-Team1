2375088470ce7af3fe2037a6e28bbe4ebbd85d14
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-03-24 11:14:42 +0900

    refactor(nav): organize structure + add testing

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,35 +1,47 @@
 import { Link, useLocation } from "react-router-dom";
 import * as style from "./navbar.css";
 
+interface LinkInterface {
+  name: string;
+  path: string;
+}
+
+export const navLinks = [
+  { name: "Home", path: "/" },
+  { name: "Summary", path: "/summary" },
+  { name: "My Page", path: "/page" },
+];
+
+const ActiveNavLink = ({ name, path }: LinkInterface) => {
+  return (
+    <Link role="link" className={style.underline} to={path}>
+      {name}
+    </Link>
+  );
+};
+
+const NavLink = ({ name, path }: LinkInterface) => {
+  return (
+    <Link role="link" className={style.navbarLink} to={path}>
+      {name}
+    </Link>
+  );
+};
+
 const NavBar = () => {
   const link = useLocation();
 
-  const navList = [
-    { name: "Home", path: "/" },
-    { name: "Summary", path: "/summary" },
-    { name: "My Page", path: "/page" },
-  ];
-
   return (
-    <div>
+    <nav>
       <ul className={style.navbarWrapper}>
-        {navList.map(({ name, path }) => {
-          console.log(path, link.pathname);
-          if (link.pathname === path) {
-            return (
-              <Link className={style.underline} key={path} to={path}>
-                {name}
-              </Link>
-            );
-          }
-          return (
-            <Link className={style.navbarLink} key={path} to={path}>
-              {name}
-            </Link>
-          );
+        {navLinks.map(({ name, path }) => {
+          const isActive = link.pathname === path;
+          if (isActive)
+            return <ActiveNavLink key={path} name={name} path={path} />;
+          return <NavLink key={path} name={name} path={path} />;
         })}
       </ul>
-    </div>
+    </nav>
   );
 };
 
