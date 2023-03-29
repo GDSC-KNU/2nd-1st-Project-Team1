5c889a8a3147c753edbef08897ed5bf84a919bdb
Yewon Cha <yewon51317@naver.com>
2023-03-29 22:22:23 +0900

    feat: add nav

--- src/components/MyPage/MyPageMenu.tsx
+++ src/components/MyPage/MyPageMenu.tsx
@@ -1,11 +1,18 @@
-
+import { Link, useLocation } from "react-router-dom";
+import { menuWrapper, menuBox, profile, menuLink, underline } from "./MyPageMenu.css";
 
 const MyPageMenu = () => {
-    return (
-        <div>
-            MyPageMenu
-        </div>
-    );
+  return (
+    <div className={menuWrapper}>
+      <div className={profile}>
+        <img src="img/knu_character.png" />
+      </div>
+      <ul className={menuBox}>
+        <li className={menuLink}>개인정보</li>
+        <li className={menuLink}>비밀번호 변경</li>
+      </ul>
+    </div>
+  );
 };
 
 export default MyPageMenu;
