249fb51e3f9c5a5e05d4a09130aac11662a9f262
Yewon Cha <yewon51317@naver.com>
2023-03-29 22:22:23 +0900

    feat: add nav

--- src/components/MyPage/MyPageMenu.css.ts
+++ src/components/MyPage/MyPageMenu.css.ts
@@ -1,1 +1,40 @@
 import { style } from "@vanilla-extract/css";
+
+export const menuWrapper = style({
+  display: "grid",
+  width: "280px",
+  margin: 0,
+  paddingTop: "20%",
+})
+
+export const menuBox = style({
+  alignItems: "center",
+  listStyle: "none",
+})
+
+export const profile = style({
+  width: "100%",
+  marginTop: "20%",
+  marginBottom: "30%",
+  borderRadius: "70%",
+  backgroundColor: "white",
+  boxShadow: "4px 4px 8px gray",
+  objectFit: "contain",
+  textAlign: "center",
+})
+
+export const menuLink = style({
+  textDecoration: "none",
+  color: "black",
+  textAlign: "center",
+  width: "100%",
+  height: "75%",
+  paddingTop: "10%",
+  fontSize: "1.4rem",
+})
+
+export const underline = style({
+  textDecoration:"underline",
+  fontWeight: "bold",
+  color: "black"
+})
