3febf4983b4fc28ad2600e71bdca2b668e72d55d
krokerdile <crazykroker@gmail.com>
2023-05-17 18:33:32 +0900

    feat: auth, home api 코드 작성

--- src/api/home.ts
+++ src/api/home.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { CourseType } from "../components/Curriculum/courseData";
 import { LoginProps } from "./auth";
+import clientApi from "./axios";
 
 // login: async ({ studentID, password }: LoginProps) => {
 //     return await axios.post(`${baseURL}/user/auth/login`, { studentID, password });
 //   },
 
-const baseURL = process.env.SERVER_URL || "http://localhost:3000";
+// const baseURL = process.env.SERVER_URL || "http://localhost:3000";
 
 interface semesterProps {
   grade: string;
@@ -15,14 +16,54 @@ interface semesterProps {
 }
 
 const homeApi = {
-  getDashboard: async ({ studentID, password }: LoginProps) => {
-    return await axios.post(`${baseURL}/fixme`, { studentID, password });
+  // getDashboard: async ({ studentID, password }: LoginProps) => {
+  //   return await axios.post(`${baseURL}/fixme`, { studentID, password });
+  // },
+  // getCourses: async (courseCondition: CourseType) => {
+  //   return await axios.post(`${baseURL}/fixme`, { ...courseCondition });
+  // },
+  // setDashboard: async (dashboard: semesterProps[]) => {
+  //   return await axios.post(`${baseURL}/fixme`, { ...dashboard });
+  // },
+
+  // 강의 조회
+  getCourses: async ({}: any) => {
+    return await clientApi.get("/lectures?lectureYear=2022");
   },
-  getCourses: async (courseCondition: CourseType) => {
-    return await axios.post(`${baseURL}/fixme`, { ...courseCondition });
+
+  //유저에 해당하는 강의 조회
+  getLectureForUser: async ({ id }: any) => {
+    return await clientApi.get(`/letures/${id}`);
   },
-  setDashboard: async (dashboard: semesterProps[]) => {
-    return await axios.post(`${baseURL}/fixme`, { ...dashboard });
+
+  //유저 강의 삭제
+  deleteLecture: async ({ id }: any) => {
+    return await clientApi.post(`/userLecture/delete/${id}`);
+  },
+
+  //유저 강의 전체 삭제
+  deleteAllLecture: async ({ token }: any) => {
+    return await clientApi.post(`/userLecture/delte/all/${token}`);
+  },
+
+  //유저 강의 추가
+  addLecture: async ({ token }: any) => {
+    return await clientApi.post(`/userLecture/add/${token}`);
+  },
+
+  //엑셀 제출
+  saveExcel: async ({ token }: any) => {
+    return await clientApi.post(`/excel/read/${token}`);
+  },
+
+  //유저 강의 불러오기
+  loadLecture: async ({ token }: any) => {
+    return await clientApi.get(`/userLecture/${token}`);
+  },
+
+  //유저 특정 강의 불러오기? 이건 뭐지?
+  loadSpecificLecture: async ({ token, LectureID }: any) => {
+    return await clientApi.get(`/userLecture/${token}/${LectureID}`);
   },
 };
 
--- src/hooks/useGetCourse.ts
+++ src/hooks/useGetCourse.ts
@@ -1,8 +1,11 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import homeApi from "../api/home";
 
 const useGetCourses = () => {
-  return useMutation(homeApi.getCourses);
+  // return useMutation(homeApi.getCourses);
+  const { data } = useQuery(["channelList"], homeApi.getCourses);
+  console.log(data);
+  return data;
 };
 
 export default useGetCourses;
